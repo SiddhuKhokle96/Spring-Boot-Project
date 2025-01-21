64d9bca84ed40c0fcbd0ad22c94d51f9bee4f93a
siddhant  khokle <120040336+SiddhuKhokle96@users.noreply.github.com>
2025-01-21 11:21:28 +0530

    Add files via upload

---
+++ CategoryService.java
@@ -0,0 +1,37 @@
+package com.example.demo.Service;
+import com.example.demo.Entity.Category;
+import com.example.demo.Repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public Page<Category> getAllCategories(Pageable pageable) {
+        return categoryRepository.findAll(pageable);
+    }
+
+    public Category createCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    public Optional<Category> getCategoryById(Long id) {
+        return categoryRepository.findById(id);
+    }
+
+    public Category updateCategory(Long id, Category category) {
+        category.setId(id);
+        return categoryRepository.save(category);
+    }
+
+    public void deleteCategory(Long id) {
+    	categoryRepository.deleteById(id);
+    }
+}
---
+++ ProductService.java
@@ -0,0 +1,38 @@
+package com.example.demo.Service;
+import com.example.demo.Entity.Product;
+import com.example.demo.Repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public Page<Product> getAllProducts(Pageable pageable) {
+        return productRepository.findAll(pageable);
+    }
+
+    public Product createProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public Optional<Product> getProductById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    public Product updateProduct(Long id, Product product) {
+        product.setId(id);
+        return productRepository.save(product);
+    }
+
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
+    }
+}
+
